68add36f126519ff6b481d0ccef8c0309d1321fc
CA2107 <cloudp@tuta.io>
2025-02-03 12:00:25 -0600

    Update task_manager.py

--- colab_leecher/utility/task_manager.py
+++ colab_leecher/utility/task_manager.py
@@ -228,7 +228,7 @@ async def Do_Leech(source, is_dir, is_ytdl, is_zip, is_unzip, is_dualzip):
         else:
             await Leech(Paths.down_path, True)
 
-    await SendLogs(False)
+    await SendLogs(True)
 
 
 async def Do_Mirror(source, is_ytdl, is_zip, is_unzip, is_dualzip):
